dc5b45f20c805ec520ceb3671ef0acc40aedb6b9
joostlek <joostlek@outlook.com>
2021-06-14 15:31:45 +0200

    Initial config

---
+++ src/main/java/hu/indicium/dev/locations/LocationApiApplication.java
@@ -0,0 +1,13 @@
+package hu.indicium.dev.locations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationApiApplication.class, args);
+    }
+
+}
