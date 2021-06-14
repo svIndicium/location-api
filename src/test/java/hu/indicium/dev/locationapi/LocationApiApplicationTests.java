05edce02b73643fe06388e59c4f04862f7051f9d
joostlek <joostlek@outlook.com>
2021-06-14 15:49:31 +0200

    Fix test

--- src/test/java/hu/indicium/dev/locationapi/LocationApiApplicationTests.java
+++ src/test/java/hu/indicium/dev/locationapi/LocationApiApplicationTests.java
@@ -1,13 +1,33 @@
 package hu.indicium.dev.locationapi;
 
+import hu.indicium.dev.locations.LocationApiApplication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class LocationApiApplicationTests {
+@DisplayName("Application")
+@Tag("Application")
+class PaymentServiceApplicationTests {
 
     @Test
+    @DisplayName("Test context loading")
     void contextLoads() {
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Test startup")
+    void main() {
+        LocationApiApplication.main(new String[]{});
+        assertThat(true).isTrue();
     }
 
 }
